c961ede47e3f9757c88ad6f83e660843216c41d5
amatteini79 <amatteini79@gmail.com>
2023-07-16 10:51:58 +0200

    insert list into flask

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+INFOS = [
+    {
+    "id" = "1",
+    "title" = "To do for first",
+    "worker" = "Tim",
+},
+    {
+    "id" = "2"
+    "title" = "To do for second"
+    "worker" = "jim"
+}
+]
+
 @app.route("/")
 def HelloWorld():
-    return render_template ('home.html')
+    return render_template ('home.html', various=INFOS)
 
 app.run(host='127.0.0.1', port=5000, debug=True)
 
