61df1b7495385578b48493ea42036f28f0cb3fa1
amatteini79 <amatteini79@gmail.com>
2023-08-01 15:21:36 +0200

    extract data

--- database.py
+++ database.py
@@ -1,2 +1,11 @@
 import sqlalchemy
+from sqlalchemy import create_engine, text
+db_connection_string = "mysql+pymysql://andrea:Gramolazzo15@localhost/mysite?charset=utf8mb4"
+engine = create_engine(db_connection_string)
+
+with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        result_all = result.all()
+        first_row = dict(result_all[0])
+        print(first_row)
 
