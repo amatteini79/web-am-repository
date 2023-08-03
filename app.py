d6dd86c907fe9ff9fd74a42278a08f14aa0b3cbc
amatteini79 <amatteini79@gmail.com>
2023-08-03 15:06:50 +0200

    before secret

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 from flask import Flask, render_template
-from database import engine
-from sqlalchemy import Text
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
 
-def load_from_db():
-with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-        jobs.append(row._asdict())
-return jobs
-
 @app.route("/")
 def HelloWorld():
-    jobs = load_from_db()
+    jobs = load_jobs_from_db()
     return render_template ('home.html', various=jobs)
 
 app.run(host='127.0.0.1', port=5000, debug=True)
--- database.py
+++ database.py
@@ -1,8 +1,16 @@
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 db_connection_string = "mysql+pymysql://andrea:Gramolazzo15@localhost/mysite?charset=utf8mb4"
 engine = create_engine(db_connection_string)
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row._asdict())
+        return jobs
 
 
         
