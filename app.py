086f28415043fd8cde9eff192a5e979d3a289af3
amatteini79 <amatteini79@gmail.com>
2023-08-09 11:12:23 +0200

    dynamic page jobs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import load_jobs_from_db
+from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def HelloWorld():
     jobs = load_jobs_from_db()
     return render_template ('home.html', various=jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
 
 app.run(host='127.0.0.1', port=5000, debug=True)
 
--- database.py
+++ database.py
@@ -1,13 +1,10 @@
 import sqlalchemy
 from sqlalchemy import create_engine, text
-import os
-from dotenv import load_dotenv  
-
-load_dotenv()
 
 
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+db_connection_string = "mysql+pymysql://andrea:Gramolazzo15@localhost/mysite?charset=utf8mb4"
 engine = create_engine(db_connection_string)
 
 def load_jobs_from_db():
@@ -18,6 +15,13 @@ def load_jobs_from_db():
             jobs.append(row._asdict())
         return jobs
 
-
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs where id = :val"),val=id)
+    rows = result.all()
+    if len(rows) == 0:
+        return None
+    else:
+        return dict(rows[0])
         
 
