8c911787554c659f6002924646cfe9ef653be7dd
amatteini79 <amatteini79@gmail.com>
2023-08-22 16:08:56 +0200

    insert data into db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -20,8 +20,10 @@ def show_job(id):
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
-    data = request.form
-    return render_template('application_submitted.html', application=data)
+    application = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, application)
+    return render_template('application_submitted.html', application=data, job=job)
 
 
 app.run(host='127.0.0.1', port=5000, debug=True)
--- database.py
+++ database.py
@@ -24,5 +24,14 @@ def load_job_from_db(id):
         return None
     else:
         return (rows[0]._asdict())
-        
-
+    
+def add_application_to_db(job_id, application):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience) VALUES (:job_id, :full_name, :linkedin, :education, :work_experience)")
+    conn.execute(query,
+                 job_id=job_id, 
+                 full_name=application['full_name'],      
+                 email=application['email'],
+                 linkedin_url=application['linkedin'],
+                 education=application['education'],
+                 work_experience=application['work_experience'],)
