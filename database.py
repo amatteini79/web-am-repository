1a3396da947df88b414f4a72a473bf2e4db15a6c
amatteini79 <amatteini79@gmail.com>
2023-08-28 15:25:23 +0200

    all data into db

--- database.py
+++ database.py
@@ -27,8 +27,8 @@ def load_job_from_db(id):
     
 def add_application_to_db(id, data):
     with engine.connect() as conn:
-        parameters = {'job_id' : id, 'full_name' : data['full_name'], 'email' : data['email']}
+        parameters = {'job_id' : id, 'full_name' : data['full_name'], 'email' : data['email'], 'linkedin' : data['linkedin'], 'education' : data['education'], 'work_experience' : data['work_experience']}
         print(parameters)
-        stmt = text("INSERT INTO applications (job_id, full_name, email) VALUES (:job_id, :full_name, :email)")
+        stmt = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience ) VALUES (:job_id, :full_name, :email, :linkedin, :education, :work_experience)")
         conn.execute(stmt, parameters)
         conn.commit()
